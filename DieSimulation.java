81b1c5ec935bb92f6e166fe0e9e8ae7b091c8e5f
Sheila Mbadi <mbadi.atieno@students.jkuat.ac.ke>
2017-04-03 11:57:58 +0300

    Used Math.random() not Random class
    More accurate than using Random class. Generates number between 0 (inclusive) and 1 (exclusive) [0,1)

--- DieSimulation.java
+++ DieSimulation.java
@@ -1,59 +1,46 @@
 package diesimulation;
 
-import java.util.Random;
 
 public class DieSimulation {
 
     public static void main(String[] args) {
-        //Random number generator to bring element of chance
-        Random rn = new Random();
-        
-        //counters to keep track of the frequency of getting the various numbers
         int counter1 = 0;
         int counter2 = 0;
         int counter3 = 0;
         int counter4 = 0;
         int counter5 = 0;
         int counter6 = 0;
-        
-        //variables for the percantages of each outcome
         double percentage1, percentage2, percentage3, percentage4, percentage5, percentage6;
 
-        //roll the dice 1000 times
         for(int i =0; i < 1000; i++) {
-            //generate 36 random numbers
-            int roll = rn.nextInt(36) + 1;
+            double roll = Math.random();
             
-            //increment appropriate counter for a specific face of the die
-            if(roll<6){
+            if(roll<1/6.0){
                 counter1++;
-            }else if(roll<12){
+            }else if(roll<1/3.0){
                 counter2++;
-            }else if(roll<18){
+            }else if(roll<0.5){
                 counter3++;
-            }else if(roll<24){
+            }else if(roll<2/3.0){
                 counter4++;
-            }else if(roll<30){
+            }else if(roll<5/6.0){
                 counter5++;
             }else {
                 counter6++;
             }     
         }
         
-        //get percentage of the faces
+        
         percentage1 = toPercent(counter1);
         percentage2 = toPercent(counter2);
         percentage3 = toPercent(counter3);
         percentage4 = toPercent(counter4);
         percentage5 = toPercent(counter5);
         percentage6 = toPercent(counter6);
-        
-        //get sum of the frequencies and percentages
         double totalPercent = percentage1+percentage2+percentage3+percentage4+percentage5+percentage6;
         double totalPercentage = Math.round(totalPercent*10)/10d;
         int totalFrequency = counter1+counter2+counter3+counter4+counter5+counter6;
         
-        //display in tabular form
         System.out.println("           Frequency    Percentage");
         System.out.println("Face 1:    " + counter1 + "          " + percentage1);
         System.out.println("Face 2:    " + counter2 + "          " + percentage2);
@@ -64,7 +51,6 @@ public class DieSimulation {
         System.out.println("           " + totalFrequency + "         " + totalPercentage);       
     }
     
-    //method to get percentage frequency of the various faces
     public static double toPercent(int frequency){
         double percent = (frequency/1000.0)*100;
         double roundedOff = Math.round(percent*10)/10d;
